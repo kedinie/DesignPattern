2eb5e0fe179622dbf786b371aaf4a6d083334fb9
niekedi <2379074979@qq.com>
2021-04-20 07:48:49 +0800

    Add Composite pattern.

---
+++ src/main/java/design/pattern/composite/CompositePatternDemo.java
@@ -0,0 +1,32 @@
+package design.pattern.composite;
+
+public class CompositePatternDemo {
+    public static void main(String[] args) {
+        Employee ceo = new Employee("John", "CEO", 30000);
+        Employee headSales = new Employee("Robert", "Head Sales", 20000);
+        Employee headMarketing = new Employee("Michel", "Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura", "Marketing", 10000);
+        Employee clerk2 = new Employee("Bob", "Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard", "Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob", "Sales", 10000);
+
+        ceo.add(headSales);
+        ceo.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        System.out.println(ceo);
+        for (Employee headEmployee : ceo.getSubordinates()) {
+            System.out.println(headEmployee);
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
