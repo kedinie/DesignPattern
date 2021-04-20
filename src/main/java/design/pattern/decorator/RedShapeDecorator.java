6ea665fd390279a6ce97e3a6608dd242179906ab
niekedi <2379074979@qq.com>
2021-04-20 21:32:25 +0800

    Add Decorator pattern.

---
+++ src/main/java/design/pattern/decorator/RedShapeDecorator.java
@@ -0,0 +1,17 @@
+package design.pattern.decorator;
+
+public class RedShapeDecorator extends ShapeDecorator{
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+
+    private void setRedBorder(Shape decoratorShape) {
+        System.out.println("Border color: Red");
+    }
+}
