a380aa4e8a2d0ab3268733bbac844f8a44654d39
niekedi <2379074979@qq.com>
2021-05-10 23:34:11 +0800

    Add transfer object pattern.

---
+++ src/main/java/design/pattern/transferObject/StudentBO.java
@@ -0,0 +1,36 @@
+package design.pattern.transferObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentBO {
+    // 列表是当做一个数据库
+    List<StudentVO> students;
+
+    public StudentBO() {
+        students = new ArrayList<>();
+        StudentVO student1 = new StudentVO("Robert", 0);
+        StudentVO student2 = new StudentVO("John", 1);
+        students.add(student1);
+        students.add(student2);
+    }
+
+    public void deleteStudent(StudentVO student) {
+        students.remove(student.getRollNo());
+        System.out.println("Student: Roll No " + student.getRollNo() + ", deleted from database");
+    }
+
+    // 从数据库中检索名单
+    public List<StudentVO> getAllStudents() {
+        return students;
+    }
+
+    public StudentVO getStudent(int rollNo) {
+        return students.get(rollNo);
+    }
+
+    public void updateStudent(StudentVO student) {
+        students.get(student.getRollNo()).setName(student.getName());
+        System.out.println("Student: Roll No " + student.getRollNo() + ", updated in database");
+    }
+}
